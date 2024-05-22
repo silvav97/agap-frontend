971f8c18170360eb422dfb3f176e046e7c131c2e
sebastian <sesilvavi@gmail.com>
2024-05-22 16:54:13 -0500

    Se ven las imagenes de los proyectos en project-list llamandolos al backend; y ya no obtengo ese error en el navegador.

--- src/app/project/pages/project-list/project-list.component.ts
+++ src/app/project/pages/project-list/project-list.component.ts
@@ -54,7 +54,7 @@ export class ProjectListComponent implements OnInit, OnDestroy {
         this.projectList = response.content;
         this.projectList.forEach(project => {
           console.log('my url_imagen es: ', project.imageUrl);
-          this.loadProjectImage(project);
+          //this.loadProjectImage(project);
           console.log('my url_imagen es: ', project.imageUrl);
         });
         this.paginator = response;
